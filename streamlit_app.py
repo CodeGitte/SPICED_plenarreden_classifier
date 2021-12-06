1bb37e8970847b0b5aaa6f7b66bac21c851a1e0a
Margit <margit.b.hain@gmail.com>
2021-12-06 16:56:21 +0100

    gitignore

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 #importing relevant libraries
 import streamlit as st
 
+#first page: entering the text 
 st.write('# Welche Partei hats gesagt?')
-
-message_text = st.text_input('Füge eine Plenarrede hier ein:')
+message_text = st.text_input('Füge hier eine Plenarrede hier ein:')
