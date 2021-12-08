5c4e1adf5733079971ccf45b33086c1eb6dba4f9
Margit <margit.b.hain@gmail.com>
2021-12-08 16:12:46 +0100

    upating

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 #importing relevant libraries
 import streamlit as st
 
+
 #first page: entering the text 
 st.write('# Welche Partei hats gesagt?')
 message_text = st.text_input('Füge hier eine Plenarrede hier ein:')
+
+#second page: output of classifier
