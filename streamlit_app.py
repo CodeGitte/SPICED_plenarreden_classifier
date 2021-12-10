3bd0581596f684d1f178889ac6eda0664d58e729
Margit <margit.b.hain@gmail.com>
2021-12-10 16:10:51 +0100

    setting up the embedding matrix and y

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,5 @@ import streamlit as st
 
 
 #first page: entering the text 
-st.write('# Welche Partei hats gesagt?')
+st.title(('Welche Partei hats gesagt?'))
 message_text = st.text_input('Füge hier eine Plenarrede hier ein:')
-
-#second page: output of classifier
