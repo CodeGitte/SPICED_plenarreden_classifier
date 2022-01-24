d83c3491bb81933d65bc39302181bb20e47bdbe1
Margit Hain <71278816+CodeGitte@users.noreply.github.com>
2022-01-24 15:28:09 +0100

    Delete streamlit_app.py

--- streamlit_app.py
+++
@@ -1,7 +0,0 @@
-#importing relevant libraries
-import streamlit as st
-
-
-#first page: entering the text 
-st.title(('Welche Partei hats gesagt?'))
-message_text = st.text_input('Füge hier eine Plenarrede hier ein:')
